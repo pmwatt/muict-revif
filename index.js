c3df812de8b68645baa000cb45446d47183cbfee
pmwatt <prachnachai.mek@student.mahidol.edu>
2023-04-22 02:03:58 +0700

    added admin insert query, no dupes check yet

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const async = require('async');
 const http = require('http');
 const express = require('express');
 const dotenv = require('dotenv');
@@ -12,10 +13,6 @@ const router = express.Router();
 app.use('/', router);
 
 // express req body for admin
-router.use(express.json());
-router.use(express.urlencoded({ extended: true }));
-
-// express req body for user
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
@@ -61,12 +58,59 @@ router.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, "/src/html/login-admin.html"));
 });
 
-router.get('/admin-management', (req,res) => {
+router.get('/admin-management', (req, res) => {
     res.statusCode = 200;
     res.sendFile(path.join(__dirname, "/src/html/admin-management.html"));
 });
 
-router.get('/commission-management', (req,res) => {
+// admin's ADD form submission
+router.post('/admin-insert-submit', (req, res) => {
+    let adminInfo = {
+        username: req.body.username,
+        fname: req.body.fn,
+        lname: req.body.ln,
+        DOB: req.body.dob,
+        phone: req.body.phone,
+        email: req.body.email
+    };
+    let adminLogin = {
+        username: req.body.username,
+        pwd: req.body.pwd,
+        adminRole: req.body.role,
+        lastLoginDate: req.body.lastLogin
+    }
+
+    if (!adminInfo || !adminLogin) {
+        return res.status(400).send({
+            error: true,
+            message: 'Please provide admin information'
+        })
+    }
+
+    // multiple queries: https://stackoverflow.com/questions/29631131/perform-two-or-more-queries-in-one-request-using-node-mysql-and-expressjs#:~:text=To%20use%20multiple%20statement%20queries%2C%20you%20should%20first,statement%20queries%20as%20shown%20below%20in%20your%20connection.query.
+    async.parallel([
+        () => {
+            connection.query("INSERT INTO admin_info SET ?", adminInfo, (error, results) => {
+                if (error) throw error;
+                return res.send({
+                    error: false,
+                    data: results.affectedRows,
+                    message: 'New admin info record been added successfully.'
+                })
+            }),
+            connection.query("INSERT INTO admin_login SET ?", adminLogin, (error, results) => {
+                if (error) throw error;
+                return res.send({
+                    error: false,
+                    data: results.affectedRows,
+                    message: 'New admin login record has been added successfully.'
+                })
+            })
+        }
+    ])
+});
+
+router.get('/commission-management', (req, res) => {
     res.statusCode = 200;
     res.sendFile(path.join(__dirname, "/src/html/commission-management.html"));
 });
