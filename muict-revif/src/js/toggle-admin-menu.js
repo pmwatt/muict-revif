11fdf3b7f0f5e08ed0bd490d9ac104418bf13280
pmwatt <prachnachai.mek@student.mahidol.edu>
2023-04-25 20:28:12 +0700

    renamed folders based on proj pdf

--- muict-revif/src/js/toggle-admin-menu.js
+++
@@ -1,8 +0,0 @@
-function openMenu(menu) {
-    const menus = document.getElementsByClassName("menu");
-    for (let index = 0; index < menus.length; index++) {
-        const element = menus[index];
-        element.setAttribute("style", "display: none");
-    }
-    document.getElementById(menu).setAttribute("style", "display: flex");
-}
